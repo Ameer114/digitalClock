1437bb6dfcdc3524b0a5d0b913953c63ba07e6a2
Ameer Nagarasi <124485878+Ameer114@users.noreply.github.com>
2025-07-04 01:43:26 +0530

    Update Clock.jsx

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -4,6 +4,11 @@ import {use, useEffect, useState} from 'react'
 const Clock = () => {
     const [time,settime]=useState(new Date());
  
+       const options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true, // This ensures 12-hour format with AM/PM
+      };
 useEffect(()=>{
   const inteerval=  setInterval(() => {
          settime(new Date())
@@ -14,10 +19,36 @@ useEffect(()=>{
    }) 
 },[])
 
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen()
+      .catch(err => {
+        console.error(`Error attempting to enable fullscreen: ${err.message} (${err.name})`);
+      });
+  } else {
+    document.exitFullscreen();
+  }
+}
+
   return (
+    <>
     <div className='text'>
-      {time.getHours()>12  && time.getHours()<10 ? "0"+time.toLocaleTimeString() : " " +time.toLocaleTimeString()}
+      <span className='bigblue'>const time</span> = &#123;
+      <br />
+      &nbsp; <span className='blue'>Hour:</span>{time.toLocaleTimeString('en-US', options).split(":")[0] <10?'0'+time.toLocaleTimeString('en-US', options).split(":")[0]:time.toLocaleTimeString('en-US', options).split(":")[0] },
+      <br />
+       &nbsp; <span className='blue'>Minutes:</span>{time.getMinutes()<10? '0'+time.getMinutes():time.getMinutes()},
+       <br />
+        &nbsp; <span className='blue'>Seconds:</span>{time.getSeconds()<10? '0'+time.getSeconds():time.getSeconds()},
+        <br />
+        &nbsp; <span className='blue'>Meridian:</span> <span className='orange'>"{time.toLocaleTimeString('en-US', options).split(" ")[1]}"</span>
+      <br />
+      &#125;;
+      <br />
+      
     </div>
+    {!document.fullscreenElement?<button className='btn' onClick={toggleFullscreen}>▶️</button>:""}
+    </>
   )
 }
 
